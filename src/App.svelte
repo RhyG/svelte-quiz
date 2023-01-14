5310313ea60dd05851213a038101dbc11bf31159
rhyg <rhysgeary@gmail.com>
2023-01-14 22:12:37 +1000

    CSS basics.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
-	import Quiz from './Quiz.svelte';
+  import Quiz from "./Quiz.svelte";
 </script>
 
 <h1>My first Svelte site</h1>
 <Quiz quizName="New Quiz" />
+
+<style>
+  :global(h4) {
+    color: blue;
+  }
+</style>
--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
-    type Answer = "a" | "b" | "c" | "d";
+  type Answer = "a" | "b" | "c" | "d";
 
-    let result = "";
-    let correctAnswer: Answer = "b";
+  let result = "";
+  let correctAnswer: Answer = "b";
 
-    let answers: Answer[] = [ "a", "b", "c", "d" ]
+  let answers: Answer[] = ["a", "b", "c", "d"];
 
-    function pickAnswer(answer: Answer) {
-        console.log('Picked', answer);
-        if (answer === correctAnswer) {
-            result = "Correct"!
-            return;
-        }
-
-        result = "Wrong";
-        return;
+  function pickAnswer(answer: Answer) {
+    if (answer === correctAnswer) {
+      result = "Correct"!;
+      return;
     }
+
+    result = "Wrong";
+    return;
+  }
 </script>
 
 <div>
-    {#if result}
-        <h4>{result}</h4>
-    {:else}
-        <h4>Choose an answer</h4>
-    {/if}
+  {#if result}
+    <h4 class={`${result === "Correct" ? "correct" : "incorrect"}`}>{result}</h4>
+  {:else}
+    <h4>Choose an answer</h4>
+  {/if}
 
-    {#each answers as answer}
-        <button on:click={() => pickAnswer(answer)}>Answer {answer.toUpperCase()}</button>
-    {/each}
+  {#each answers as answer}
+    <button on:click={() => pickAnswer(answer)}>Answer {answer.toUpperCase()}</button>
+  {/each}
 </div>
+
+<style>
+  .correct {
+    color: green;
+  }
+
+  .incorrect {
+    color: red;
+  }
+</style>
