5f8fd1e0895fa61dab81fda7eb0425adf92944e0
rhyg <rhysgeary@gmail.com>
2023-01-14 14:40:37 +1000

    Inputs, binding and reactivity

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,7 +1,24 @@
 <script>
     export let quizName = "Rhys Quiz";
+    let presses = 0;
+
+    // let title = ""
+    let a = 0;
+    let b = 0;
+
+    function onClick() {
+        presses += 1
+    }
 </script>
 
 <div>
     <h2>{quizName}</h2>
+    <!-- <h4>{title}</h4>
+    <input bind:value={title} type="text"> -->
+    
+    <input type="number" bind:value={a}>
+    <input type="number" bind:value={b}>
+    <h4>{a + b}</h4>
+    <button on:click={onClick}>Press me</button>
+    <p>Number of presses: {presses}</p>
 </div>
