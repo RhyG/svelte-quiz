12bc5e6670f19a013b4b1de7d16e3f3c45a9baa1
rhyg <rhysgeary@gmail.com>
2023-01-15 06:33:32 +1000

    Await.

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -23,22 +23,31 @@
     const quiz = await res.json();
     return quiz;
   }
+
+  function handleClick() {
+    quiz = getQuiz();
+  }
 </script>
 
 <div>
-  <button on:click={getQuiz}>Get quiz</button>
+  <button on:click={handleClick}>Get quiz</button>
   {#if result}
     <h4 class={`${result === "Correct" ? "correct" : "incorrect"}`}>{result}</h4>
   {:else}
     <h4>Choose an answer</h4>
   {/if}
 
+  {#await quiz}
+    <p>Loading...</p>
+  {:then data}
+    <h3>{data.results[0].question}</h3>
+  {/await}
+
   {#each answers as answer}
     <button on:click={() => pickAnswer(answer)}>Answer {answer.toUpperCase()}</button>
   {/each}
 </div>
 
-<!-- https://opentdb.com/api.php?amount=10&category=23&type=multiple -->
 <style>
   .correct {
     color: green;
